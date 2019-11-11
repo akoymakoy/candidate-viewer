eb1e589e6661513a18950d34f7d99a55fb808379
Mark Miraflor <Mark.Miraflor@sinch.com>
2019-11-11 13:36:14 +0100

    working hard coded values

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module candidate-viewer
 
 go 1.13
 
-require github.com/go-sql-driver/mysql v1.4.1
+require (
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/gorilla/mux v1.7.3
+)
--- main.go
+++ main.go
@@ -1,143 +1,56 @@
-// Package main starts the simple server on port and serves HTML,
-// CSS, and JavaScript to clients.
 package main
 
+
 import (
-	"candidate-viewer/candidateutil"
-	"candidate-viewer/database"
-	"fmt"
-	"html/template"
-	"log"
+    "html/template"
+    "log"
 	"net/http"
-	"os"
-	"strconv"
 	"time"
+    
+     "github.com/gorilla/mux"
 )
 
-// templates parses the specified templates and caches the parsed results
-// to help speed up response times.
-var templates = template.Must(template.ParseFiles("./templates/base.html", "./templates/body.html"))
-
-// logging is middleware for wrapping any handler we want to track response
-// times for and to see what resources are requested.
-func logging(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		req := fmt.Sprintf("%s %s", r.Method, r.URL)
-		log.Println(req)
-		next.ServeHTTP(w, r)
-		log.Println(req, "completed in", time.Now().Sub(start))
-	})
-}
-
-// index is the handler responsible for rending the index page for the site.
-func index() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var id int
-		var err error
-		if r.Method == http.MethodPost {
-			id,err = strconv.Atoi(r.FormValue("Id"))
-			log.Print("POST")
-		}else{
-			id =1
-			log.Print("REQUEST INDEX")
-		}
-
-
-		db,err := database.Connect()
-		c := candidateutil.Get(db,id)
-
-		b := struct {
+func parseTpl(w http.ResponseWriter, r *http.Request) {
+	b := struct {
 			Title        template.HTML
-			Id			 int
+			Id,Javaskill			 int
 			Name,Email   string
 		}{
 			Title:        template.HTML("Business &verbar; Landing"),
-			Id: 		c.Id,
-			Name:       c.Name,
-			Email:       c.Email,
+			Id: 		1,
+			Javaskill: 100,
+			Name:       "Mark Miraflor",
+			Email:       "mark.miraflor@luunax.com",
 		}
-		templates.ExecuteTemplate(w, "base", &b)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("index: couldn't parse template: %v", err), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-	})
-}
+		
+	// Files are provided as a slice of strings.
+	paths := []string{
+		"templates/template1.tmpl",
+	}
 
-// public serves static assets such as CSS and JavaScript to clients.
-func public() http.Handler {
-	return http.StripPrefix("/public/", http.FileServer(http.Dir("./public")))
-}
+	contactTmpl, _ := template.ParseFiles(paths...)
 
-//candidate view
-func profilepage(design int) http.Handler{
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var id int
-		var err error
-		if r.Method == http.MethodPost {
-			id,err = strconv.Atoi(r.FormValue("Id"))
-			log.Print("POST")
-		}else{
-			id =1
-			log.Print("REQUEST VIEW1")
-		}
-		db,err := database.Connect()
-		c := candidateutil.Get(db,id)
-
-		b := struct {
-			Title        template.HTML
-			Id			 int
-			Name,Email   string
-		}{
-			Title:        template.HTML("Business &verbar; Landing"),
-			Id: 		c.Id,
-			Name:       c.Name,
-			Email:       c.Email,
-		}
-		if design == 1{
-			log.Print("GOING TO TEMPLATE 1")
-			templates.ExecuteTemplate(w, "template1", &b)
-		}else{
-			log.Print("GOING TO TEMPLATE 2")
-			templates.ExecuteTemplate(w, "template2", &b)
-		}
-		if err != nil {
-			http.Error(w, fmt.Sprintf("index: couldn't parse template: %v", err), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-	})
+	err := contactTmpl.ExecuteTemplate(w, "layout", b)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	
+	 r := mux.NewRouter()
 
-	mux := http.NewServeMux()
-	mux.Handle("/public/", logging(public()))
-	mux.Handle("/view1/", logging(profilepage(1)))
-	mux.Handle("/view2/", logging(profilepage(2)))
-	//mux.Handle("/", logging(index()))
+	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("./public/assets/styles"))))
+	r.PathPrefix("/js/").Handler(http.StripPrefix("/js/", http.FileServer(http.Dir("./public/assets/scripts"))))
 
-	port, ok := os.LookupEnv("PORT")
-	if !ok {
-		port = "8080"
-	}
+	r.HandleFunc("/", parseTpl)
 
-	addr := fmt.Sprintf(":%s", port)
-	server := http.Server{
-		Addr:         addr,
-		Handler:      mux,
-		ReadTimeout:  15 * time.Second,
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8080",
 		WriteTimeout: 15 * time.Second,
-		IdleTimeout:  15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
 
-	http.ListenAndServe(addr, mux)
-
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("main: couldn't start simple server: %v\n", err)
-	}
-
-
+	log.Fatal(srv.ListenAndServe())
 }
